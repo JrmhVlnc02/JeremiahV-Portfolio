70d5edb50d8c6983881e18737312605c9310a6e9
JrmhVlnc02 <95947992+JrmhVlnc02@users.noreply.github.com>
2023-10-25 21:28:29 +0800

    update work

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -119,25 +119,24 @@ const WorkSlider = () => {
         workSlider.slides.map((slide, index) => {
           return (
             <SwiperSlide key={index}>
-              <div className='grid grid-cols-2 grid-rows-1 gap-5 cursor-pointer'>
+              <div className='grid grid-cols-2 grid-rows-1 gap-4 cursor-pointer'>
                 {slide.images.map((image, index) => {
                   return (
                     <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={index}>
                       <div className='flex items-center justify-center relative overflow-hidden group'/>
                         
-                        <Image src={image.path} width={250} height={600} alt='' className='rounded-lg shadow-xl'/>
+                        <Image src={image.path} width={250} height={600} alt='' className='rounded-lg shadow-xl transform transition duration-500 hover:scale-125 flex justify-center items-center'/>
                         {/* overlay gradient */}
-                        <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-50 transition-all duration-700 '>                                             
-                        </div>
+                        {/* <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-30 transition-all duration-700 '></div> */}
                         {/* title */}
                         <div className='absolute bottom-0 translate-y-full group-hover:translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 '>
                           
                           <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
                             
                             
-                            <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
+                            {/* <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
                               <BsArrowRight/>
-                            </div>
+                            </div> */}
                             
                           </div>
 
